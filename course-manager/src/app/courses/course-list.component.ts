0bb06f070ec27b104d36662534f1f4ca28d5d902
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-07-19 23:22:32 -0300

    Criando componentes e construindo o projeto

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { isNull } from 'util';
+import { Course } from './course';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html',
+})
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                'id': 1,
+                'name': 'Matematica',
+                'imageUrl': '',
+                'price': 99.99,
+                'code': 'xps-345',
+                'duration': 120,
+                'rating': 4.4,
+                'releaseDate': 'December, 2, 2019'
+            },
+            {
+                'id': 2,
+                'name': 'Portugues',
+                'imageUrl': '',
+                'price': 45.99,
+                'code': 'gds-345',
+                'duration': 80,
+                'rating': 4.5,
+                'releaseDate': 'December, 4, 2019'
+            }
+        ]
+    }
+}
