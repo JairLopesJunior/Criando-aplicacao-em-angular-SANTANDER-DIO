8682586eaf7dc4bd14338c1502e54001893052d6
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-08 14:41:01 -0300

    Tratando eventos com data binding

--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -22,7 +22,7 @@ var COURSES: Course[] = [
         code: 'XLF-1212',
         rating: 3,
         price: 12.99,
-        imageUrl: '/assets/images/cli.png',
+        imageUrl: '/assets/images/mario.png',
     },
     {
         id: 2,
@@ -33,7 +33,7 @@ var COURSES: Course[] = [
         code: 'DWQ-3412',
         rating: 3.5,
         price: 24.99,
-        imageUrl: '/assets/images/forms.png',
+        imageUrl: '/assets/images/mario.png',
     },
     {
         id: 3,
@@ -44,7 +44,7 @@ var COURSES: Course[] = [
         code: 'QPL-0913',
         rating: 4.0,
         price: 36.99,
-        imageUrl: '/assets/images/http.png',
+        imageUrl: '/assets/images/mario.png',
     },
     {
         id: 4,
@@ -55,7 +55,7 @@ var COURSES: Course[] = [
         code: 'OHP-1095',
         rating: 4.5,
         price: 46.99,
-        imageUrl: '/assets/images/router.png',
+        imageUrl: '/assets/images/mario.png',
     },
     {
         id: 5,
@@ -66,6 +66,6 @@ var COURSES: Course[] = [
         code: 'PWY-9381',
         rating: 5,
         price: 56.99,
-        imageUrl: '/assets/images/animations.png',
+        imageUrl: '/assets/images/mario.png',
     }
 ];
