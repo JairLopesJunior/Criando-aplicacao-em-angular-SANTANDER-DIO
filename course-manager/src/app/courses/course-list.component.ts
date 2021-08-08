c4a23b205fb520268954e3e46911c923b4b314f3
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-07 23:02:13 -0300

    Aprendendo Injeção de Dependencias no ANgular

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -8,28 +9,9 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit{
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService){}
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                'id': 1,
-                'name': 'Matematicaa',
-                'imageUrl': '/assets/images/mario.png',
-                'price': 99.99,
-                'code': 'xps-345',
-                'duration': 120,
-                'rating': 4.4,
-                'releaseDate': 'December, 2, 2019'
-            },
-            {
-                'id': 2,
-                'name': 'Portugues',
-                'imageUrl': '/assets/images/mario.png',
-                'price': 45.99,
-                'code': 'gds-345',
-                'duration': 80,
-                'rating': 4.5,
-                'releaseDate': 'December, 4, 2019'
-            }
-        ]
+        this.courses = this.courseService.retriveAll();
     }
 }
