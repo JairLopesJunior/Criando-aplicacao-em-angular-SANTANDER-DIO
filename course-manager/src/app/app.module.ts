bb68f7c0655d5c29d4742d4dc3ca57f7190aa07c
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-08-11 23:56:46 -0300

    Conhecendo a pasta shared

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './shared/pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseModule } from './courses/course.module';
 
